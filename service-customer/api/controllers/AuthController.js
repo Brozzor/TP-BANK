bbdac5b38617ac71cd484038b68490149a53bc8f
Bromain <377roro@gmail.com>
2023-11-17 16:09:26 +0100

    amelioration de l'authentification

--- service-customer/api/controllers/AuthController.js
+++ service-customer/api/controllers/AuthController.js
@@ -2,17 +2,17 @@ const randomstring = require('randomstring')
 module.exports = {
     login: async (req, res) => {
         const customer = await Customer.findOne({email: req.body.email})
-        let token
+
         try {
             if (!customer) throw new Error('Unauthorized')
             const isMatch = Customer.comparePassword(req.body.password, customer.password)
             if (!isMatch) throw new Error('Unauthorized')
-            token = randomstring.generate(75)
+            const token = randomstring.generate(75)
             await Customer.update({id: customer.id}, {token})
+            return res.json(token)
         } catch (error) {
             return res.status(401).json({ error: error.message })
         }
 
-        return res.json(token)
     }
 }
